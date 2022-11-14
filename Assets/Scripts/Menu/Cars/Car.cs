05e63c2c8ef5cf051586e6b3a65f83340f26ffa9
mkovalevwork <mkovalevwork@gmail.com>
2022-11-14 18:50:17 +0300

    + interface

--- Assets/Scripts/Menu/Cars/Car.cs
+++ Assets/Scripts/Menu/Cars/Car.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Car : MonoBehaviour
+public class Car : MonoBehaviour, IMovable
 {
     [SerializeField] protected string carName;
     [SerializeField] protected float carSpeedForward;
@@ -9,4 +9,9 @@ public class Car : MonoBehaviour
     public string CarName { get { return carName; } }  //readonly
     public float CarSpeedForward { get { return carSpeedForward; } }  //readonly
     public float CarSpeedRotation { get { return carSpeedRotation; } }  //readonly
+
+    public void SuperPower()
+    {
+        
+    }
 }
